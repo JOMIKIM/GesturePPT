61eb0b65d0b54d5897b0385a022def291d9bc858
vwaiolet <passwordk@kakao.com>
2023-06-01 20:12:38 +0900

    add action

--- ppt_cmd.py
+++ ppt_cmd.py
@@ -1,26 +1,24 @@
-import cv2
-import mediapipe as mp
-import numpy as np
+from cv2 import flip, cvtColor, cvtColor, COLOR_BGR2RGB, COLOR_RGB2BGR, imshow, waitKey, VideoCapture, putText, FONT_HERSHEY_SIMPLEX
+from mediapipe import solutions
+from numpy import zeros, linalg, newaxis, arccos, einsum, degrees, concatenate, expand_dims, array, float32, argmax
 from tensorflow.keras.models import load_model
+from win32com.client import Dispatch
+from pyautogui import getWindowsWithTitle, keyDown
 
-import win32com.client
-import pyautogui
-
-
-actions = ['next', 'back']
+actions = ['next', 'back', 'pgup', 'pgdn']
 seq_length = 30
 
 model = load_model('models/model.h5')
 
 # MediaPipe hands model
-mp_hands = mp.solutions.hands
-mp_drawing = mp.solutions.drawing_utils
+mp_hands = solutions.hands
+mp_drawing = solutions.drawing_utils
 hands = mp_hands.Hands(
     max_num_hands=1,
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5)
 
-cap = cv2.VideoCapture(0)
+cap = VideoCapture(0)
 
 seq = []
 action_seq = []
@@ -31,14 +29,14 @@ while cap.isOpened():
     if not ret:
         break
 
-    img = cv2.flip(img, 1)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = flip(img, 1)
+    img = cvtColor(img, COLOR_BGR2RGB)
     result = hands.process(img)
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    img = cvtColor(img, COLOR_RGB2BGR)
 
     if result.multi_hand_landmarks is not None:
         for res in result.multi_hand_landmarks:
-            joint = np.zeros((21, 4))
+            joint = zeros((21, 4))
             for j, lm in enumerate(res.landmark):
                 joint[j] = [lm.x, lm.y, lm.z, lm.visibility]
 
@@ -47,16 +45,16 @@ while cap.isOpened():
             v2 = joint[[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], :3] # Child joint
             v = v2 - v1 # [20, 3]
             # Normalize v
-            v = v / np.linalg.norm(v, axis=1)[:, np.newaxis]
+            v = v / linalg.norm(v, axis=1)[:, newaxis]
 
             # Get angle using arcos of dot product
-            angle = np.arccos(np.einsum('nt,nt->n',
+            angle = arccos(einsum('nt,nt->n',
                 v[[0,1,2,4,5,6,8,9,10,12,13,14,16,17,18],:], 
                 v[[1,2,3,5,6,7,9,10,11,13,14,15,17,18,19],:])) # [15,]
 
-            angle = np.degrees(angle) # Convert radian to degree
+            angle = degrees(angle) # Convert radian to degree
 
-            d = np.concatenate([joint.flatten(), angle])
+            d = concatenate([joint.flatten(), angle])
 
             seq.append(d)
 
@@ -65,11 +63,11 @@ while cap.isOpened():
             if len(seq) < seq_length:
                 continue
 
-            input_data = np.expand_dims(np.array(seq[-seq_length:], dtype=np.float32), axis=0)
+            input_data = expand_dims(array(seq[-seq_length:], dtype=float32), axis=0)
 
             y_pred = model.predict(input_data).squeeze()
 
-            i_pred = int(np.argmax(y_pred))
+            i_pred = int(argmax(y_pred))
             conf = y_pred[i_pred]
 
             if conf < 0.9:
@@ -87,22 +85,18 @@ while cap.isOpened():
 
                 if last_action != this_action:
                     if this_action == 'next':
-                        ppt = win32com.client.Dispatch("PowerPoint.Application")
-                        active_presentation = ppt.ActivePresentation
-                        window = pyautogui.getWindowsWithTitle('pptx')[0]
-                        window.activate()
-                        pyautogui.hotkey('ctrl', 'pgdn')
+                        keyDown('right')
                     elif this_action == 'back':
-                        ppt = win32com.client.Dispatch("PowerPoint.Application")
-                        active_presentation = ppt.ActivePresentation
-                        window = pyautogui.getWindowsWithTitle('pptx')[0]
-                        window.activate()
-                        pyautogui.hotkey('ctrl', 'pgup')
+                        keyDown('left')
+                    elif this_action == 'pgup':
+                        keyDown('pgup')
+                    elif this_action == 'pgdn':
+                        keyDown('pgdn')
 
                     last_action = this_action
 
-            cv2.putText(img, f'{this_action.upper()}', org=(int(res.landmark[0].x * img.shape[1]), int(res.landmark[0].y * img.shape[0] + 20)), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
+            putText(img, f'{this_action.upper()}', org=(int(res.landmark[0].x * img.shape[1]), int(res.landmark[0].y * img.shape[0] + 20)), fontFace=FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
 
-    cv2.imshow('img', img)
-    if cv2.waitKey(1) == ord('q'):
+    imshow('img', img)
+    if waitKey(1) == ord('q'):
         break
